ea5dd6ba1ca9e0c5cff8b11baecf251a1cf55622
yeonjy73 <yeon703@naver.com>
2024-11-10 18:41:47 +0900

    feat: 유저 감정 분석전달 api

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 # emotions 라우터 import
 from src.routers.emotions import router as emotions_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(
+    title="MoodTune",
+    description="MoodTune의 API 문서입니다.",
+)
+
 app.include_router(emotions_router)  # 라우터 포함
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처 허용
+    allow_credentials=True, # 쿠키와 인증 정보를 포함할 수 있도록 설정
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 @app.get("/")
 async def root():
--- src/routers/emotions.py
+++ src/routers/emotions.py
@@ -1,14 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+from fastapi.responses import JSONResponse
 from src.models.emotion import Emotion
+from src.DetectEmotion import GetDominantEmotion
+from pydantic import BaseModel
 
 router = APIRouter()
 
-'''
-@router.get("/api/test/")
-async def create_item_draft():
-    return {"emotion": "happy"}
-'''
+class ImagePathRequest(BaseModel):
+    img_path: str
 
-@router.post("/api/emotions/")
-async def create_item(emotion: Emotion):
-    return {"emotion": emotion}
+@router.post("/api/emotion", description= "감정 인식 결과 전달",
+        responses={
+            200: {
+                "content": {
+                "application/json": {
+                    "example": {
+                        "emotion": "dominant_emotion"}
+                }
+            }
+            }
+        })
+async def detect_emotion(request: ImagePathRequest):
+    dominant_emotion = GetDominantEmotion(src=request.img_path)
+    return JSONResponse(content={"emotion": dominant_emotion})
