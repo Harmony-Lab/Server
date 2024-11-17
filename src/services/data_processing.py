8ba8759b5b5c3de3ca7d7fe7c5a609c1e293a4dc
yeonjy73 <yeon703@naver.com>
2024-11-17 13:22:58 +0900

    feat: 데이터 전처리- 감정별로 파일 생성

--- src/services/data_processing.py
+++ src/services/data_processing.py
@@ -8,11 +8,20 @@ PARENT_DIR = os.path.dirname(PARENT_DIR)  # ai
 
 # CSV 파일 경로 설정
 CSV_FILE_PATH = os.path.join(PARENT_DIR, "data", "emotion_labelled_song_dataset.csv")  # 원본 CSV 파일의 절대 경로
-OUTPUT_CSV_PATH = os.path.join(PARENT_DIR, "data", "filtered_data.csv")  # 출력 CSV 파일의 절대 경로
 
-# data 패키지에 filtered_data.csv 만듦
+# 라벨에 따른 파일 이름 매핑
+label_to_filename = {
+    '0': 'filtered_data_sad.csv',
+    '1': 'filtered_data_happy.csv',
+    '2': 'filtered_data_energetic.csv',
+    '3': 'filtered_data_calm.csv'
+}
+
+# data 패키지에 필터링된 CSV 파일을 만듦
 def process_csv():
-    data = []
+    # 각 레이블에 해당하는 데이터를 저장할 딕셔너리
+    data_dict = {key: [] for key in label_to_filename.keys()}
+    
     try:
         # 원본 CSV 파일 읽기
         with open(CSV_FILE_PATH, mode='r', encoding='utf-8') as csv_file:
@@ -20,14 +29,21 @@ def process_csv():
             for row in reader:
                 # 'labels'와 'uri' 열만 추출
                 if 'labels' in row and 'uri' in row:
-                    data.append({'labels': row['labels'], 'uri': row['uri']})
+                    # uri 값 처리: 'spotify:track:' 제거하고 새로운 URL 추가
+                    uri = row['uri'].replace('spotify:track:', 'https://open.spotify.com/track/')
+                    # labels에 따라 적절한 리스트에 추가
+                    if row['labels'] in data_dict:
+                        data_dict[row['labels']].append({'uri': uri})
 
-        # 새로운 CSV 파일로 저장
-        with open(OUTPUT_CSV_PATH, mode='w', encoding='utf-8', newline='') as output_file:
-            fieldnames = ['labels', 'uri']
-            writer = csv.DictWriter(output_file, fieldnames=fieldnames)
-            writer.writeheader()  # 헤더 작성
-            writer.writerows(data)  # 데이터 작성
+        # 각 레이블에 따른 CSV 파일로 저장
+        for label, data in data_dict.items():
+            if data:  # 데이터가 있는 경우에만 파일 생성
+                output_file_path = os.path.join(PARENT_DIR, "data", label_to_filename[label])
+                with open(output_file_path, mode='w', encoding='utf-8', newline='') as output_file:
+                    fieldnames = ['uri']
+                    writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+                    #writer.writeheader()  # 헤더 작성
+                    writer.writerows(data)  # 데이터 작성
 
     except Exception as e:
         print(f"Error: {str(e)}")
