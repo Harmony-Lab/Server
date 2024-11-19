088abc1b7e8f654c0bf38a2001e1a770512e44c6
yeonjy73 <yeon703@naver.com>
2024-11-19 18:50:11 +0900

    fix: swagger 문서 수정, create-session 수정

--- src/routers/playlists.py
+++ src/routers/playlists.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Cookie, HTTPException
 from src.services.spotify import create_playlist  # Spotify 서비스에서 생성한 함수 가져오기
 from src.routers.users import get_user
+from src.models.playlist import Playlist
 
 router = APIRouter()
 PLAYLIST_SIZE = 5
 
-@router.post("/api/playlists/")
+@router.post("/api/playlists/", response_model=Playlist)
 async def create_user_playlist(session_id: str = Cookie(None)):
     # 사용자 데이터 조회
     user_data = await get_user(session_id)
--- src/routers/users.py
+++ src/routers/users.py
@@ -19,22 +19,41 @@ def create_session():
     return str(uuid4())
 
 # 사용자 세션 생성 + 세션값 반환
-@router.get("/api/users/create-session")
-async def create_user_session(session_id: str = Cookie(None), response: Response = None):
-    # 세션 ID가 없으면 새로운 사용자 데이터 생성
-    if not session_id or session_id not in user_data_store:
-        session_id = create_session()  # 새로운 세션 ID 생성
-        user_data_store[session_id] = User(emotion=None, playlist=None) # 새로운 user 생성 - 미완
+@router.get("/api/users/create-session",
+            responses={
+            200: {
+                "content": {
+                "application/json": {
+                    "example": {
+                        "session_id": "user_session_id"}
+                }
+            }
+            }
+        })
+async def create_user_session(response: Response = None):
+    
+    session_id = create_session()  # 새로운 세션 ID 생성
+    user_data_store[session_id] = User(emotion=None, playlist=None)
         
-        # 쿠키에 HttpOnly 속성을 추가하여 JavaScript에서 접근할 수 없게 만듦
-        response.set_cookie(key="session_id", value=session_id, httponly=True, path="/")
+    # 쿠키에 HttpOnly 속성을 추가하여 JavaScript에서 접근할 수 없게 만듦
+    response.set_cookie(key="session_id", value=session_id, httponly=True, path="/")
         
     # 사용자의 세션ID 반환
     return {"session_id": session_id}
 
 # Restart : 사용자 세선 삭제 후 새로운 세션 데이터 생성
-@router.get("/api/users/reset-session")
-async def reset_session(session_id: str = Cookie(None), response: Response = None):
+@router.get("/api/users/restart-session",
+            responses={
+            200: {
+                "content": {
+                "application/json": {
+                    "example": {
+                        "session_id": "new_user_session_id"}
+                }
+            }
+            }
+        })
+async def restart_session(session_id: str = Cookie(None), response: Response = None):
     # 세션 ID가 없거나 유효하지 않으면 오류 반환
     await get_user(session_id)
     
@@ -49,6 +68,6 @@ async def reset_session(session_id: str = Cookie(None), response: Response = Non
     return {"session_id": new_session_id}
       
 # 세션으로 사용자 데이터 조회
-@router.get("/api/users/")
+@router.get("/api/users/", response_model=User)
 async def get_user_data(session_id: Optional[str] = Cookie(None)):
     return await get_user(session_id) 
