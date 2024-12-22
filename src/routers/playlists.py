2e24b80265f4e0873eb5cf0764a8c9a76123e196
yeonjy73 <yeon703@naver.com>
2024-12-22 18:07:01 +0900

    fix

--- src/routers/playlists.py
+++ src/routers/playlists.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Cookie
+from fastapi import APIRouter, HTTPException, Header
 from src.services.spotify import create_playlist  # Spotify 서비스에서 생성한 함수 가져오기
 from src.models.playlist import Playlist
 from src.routers.users import get_user_data  # get_user_data 메소드를 가져옵니다.
@@ -7,9 +7,11 @@ router = APIRouter()
 PLAYLIST_SIZE = 5
 
 @router.post("/", response_model=Playlist)
-async def create_user_playlist(jwtToken: str = Cookie(None)):
+async def create_user_playlist(authorization: str = Header(None)):
     # 사용자의 Playlist 생성
     try:
+        # Authorization 헤더에서 JWT 토큰 추출
+        jwtToken = authorization.split(" ")[1] if authorization else None
         user = await get_user_data(jwtToken)
         
         playlist = create_playlist(user.emotion, PLAYLIST_SIZE)
